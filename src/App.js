4c1c1d858f2b5f153e3e3c3add34df7003a16aa2
Filipe Costa <filpemdc2@gmail.com>
2022-12-24 19:09:52 -0300

    tailwindcss add + some styling

--- src/App.js
+++ src/App.js
@@ -36,21 +36,25 @@ function App() {
     setTodos(newTodos)
   }
 
-  function handleClearTodo() {
+  function handleClearTodo(e) {
+    e.preventDefault()
     const newTodos = todos.filter(todo => !todo.complete)
     setTodos(newTodos)
   }
 
   return (
-    <>
+    <div className='container mx-auto pl-32 pr-32'>
+      <h1 className='text-4xl mt-3 text-center'>To Do List</h1>
       <TodoList todos={todos} toggleComplete={toggleComplete} />
-      <form style={{ display: 'inline' }}>
-        <input ref={todoNameRef} type={"text"} />
-        <button type='submit' onClick={handleClickAdd}>Add</button>
+      <form className='inline'>
+        <input className='max-w-sm mt-12 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' ref={todoNameRef} type={"text"} />
+        <div className='flex justify-center'>
+          <button className='bg-slate-300 rounded shadow p-3 m-4' type='submit' onClick={handleClickAdd}>Add Task</button>
+          <button className='rounded shadow p-2 m-6' onClick={handleClearTodo}>Clear Complete Tasks</button>
+        </div>
       </form>
-      <button onClick={handleClearTodo}>Clear Complete Tasks</button>
       <div>{todos.filter(todo => !todo.complete).length} left To Do</div>
-    </>
+    </div>
   );
 }
 
