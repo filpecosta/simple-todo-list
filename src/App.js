4bbd39cb8695938b558a3cb7b9b875485ab5a090
Filipe Costa <filpemdc2@gmail.com>
2022-12-24 18:27:13 -0300

    todo with localStorage add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './components/TodoList';
 import { v4 as uuidv4 } from 'uuid';
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
   const todoNameRef = useRef()
-  console.log("🚀 ~ file: App.js:7 ~ App ~ todoNameRef", todoNameRef)
   const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    console.log("🚀 ~ file: App.js:14 ~ useEffect ~ storedTodos", storedTodos)
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function handleClickAdd(e) {
     const name = todoNameRef.current.value
     if (name === '') return
-    console.log('Todo:', name)
     setTodos(prevTodos => {
       return [...prevTodos, { id: uuidv4, name: name, complete: false }]
     })
