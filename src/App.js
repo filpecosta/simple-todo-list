7baabcf41b54329197e9b37ba9fa4b51ec090c05
Filipe Costa <filpemdc2@gmail.com>
2022-12-25 00:09:55 -0300

    title

--- src/App.js
+++ src/App.js
@@ -48,25 +48,28 @@ function App() {
   }
 
   return (
-    <div className='px-4 md:px-32'>
-      <h1 className='text-4xl mt-3 text-center'>To Do List</h1>
-      <div className='px-24'>
-        <TodoList todos={todos} toggleComplete={toggleComplete} />
+    <>
+      <title>Todo List - made by Filipe Leonardo</title>
+      <div className='px-4 md:px-32'>
+        <h1 className='text-4xl mt-3 text-center'>To Do List</h1>
+        <div className='px-24'>
+          <TodoList todos={todos} toggleComplete={toggleComplete} />
 
+        </div>
+        <form className=''>
+          <div className='flex justify-center'>
+            <input className='max-w-sm mt-12 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' ref={todoNameRef} type={"text"} />
+
+          </div>
+          <div className='flex justify-center'>
+            <button className='bg-slate-300 rounded shadow p-3 m-4 w-44' type='submit' onClick={handleClickAdd}>Add Task</button>
+            <button className='bg-green-300 rounded shadow p-2 m-6' onClick={handleClearTodo}>Clear Complete Tasks</button>
+          </div>
+        </form>
+        <div className='flex justify-end'>{todos.filter(todo => !todo.complete).length} left To Do</div>
+        <Footer />
       </div>
-      <form className=''>
-        <div className='flex justify-center'>
-          <input className='max-w-sm mt-12 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' ref={todoNameRef} type={"text"} />
-
-        </div>
-        <div className='flex justify-center'>
-          <button className='bg-slate-300 rounded shadow p-3 m-4 w-44' type='submit' onClick={handleClickAdd}>Add Task</button>
-          <button className='bg-green-300 rounded shadow p-2 m-6' onClick={handleClearTodo}>Clear Complete Tasks</button>
-        </div>
-      </form>
-      <div className='flex justify-end'>{todos.filter(todo => !todo.complete).length} left To Do</div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
