633b2c597af8988e6fb29455c0a2331a60c79a57
Filipe Costa <filpemdc2@gmail.com>
2022-12-25 00:07:32 -0300

    center footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <div className='flex justify-center'>
       Made with ❤️ by <a href='https://filipeleonardo.com/'>Filipe Leonardo</a>
     </div>
   )
